103a73103a292b8a8e6ccb799e02718b983d536a
ValouZ <debray.va@gmail.com>
2021-02-01 19:40:58 +0100

    init project and add scss folder

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Description msg="Welcome to Your Vue.js App" />
 </template>
 
+<script>
+// @ is an alias to /src
+import Description from "@/components/Description.vue";
+
+export default {
+  name: "App",
+  components: {
+    Description,
+  },
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import "@/scss/styles.scss";
+// #app {
+// }
 </style>
---
+++ src/components/Description.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="description">
+    <h1>Simple Booking</h1>
+    <p>
+      Stay in touch with our dog walkers through the chat interface. This makes
+      it easy to discuss arrangements and make bookings. Once the walk has been
+      completed you can rate your walker and book again all through the chat.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+
+<style scoped lang="scss">
+.description {
+  max-width: 311px;
+  text-align: center;
+}
+h1 {
+  font-weight: 500;
+  margin-bottom: 24px;
+  color: $main-heading;
+}
+p {
+  color: $paragraph;
+}
+</style>
